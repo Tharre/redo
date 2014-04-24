a171efa86eab35d4356826a13752b37a30a4159a
Tharre <tharre3@gmail.com>
2014-04-25 01:45:09 +0200

    Fix build.sh script

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ $CC $CFLAGS -o out/build.o -c src/build.c
 $CC $CFLAGS -o out/redo.o -c src/redo.c
 $CC $CFLAGS -o out/redo-ifchange.o -c src/redo-ifchange.c
 $CC -o out/redo out/redo.o out/util.o out/build.o $LDFLAGS
-$CC -o out/redo-ifchange out/redo.o out/util.o out/build.o $LDFLAGS
+$CC -o out/redo-ifchange out/redo-ifchange.o out/util.o out/build.o $LDFLAGS
 
 echo "Finished compiling"
