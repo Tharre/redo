a4ae9e326e31d99d0a3fd5d108345ae8cd849851
Tharre <tharre3@gmail.com>
2014-08-13 17:52:41 +0200

    Fix build for gcc

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ $CC $CFLAGS -o out/build.o -c src/build.c
 $CC $CFLAGS -o out/filepath.o -c src/filepath.c
 $CC $CFLAGS -o out/redo.o -c src/redo.c
 $CC $CFLAGS -o out/redo-ifchange.o -c src/redo-ifchange.c
-$CC $LDFLAGS -o out/redo out/redo.o out/util.o out/build.o out/filepath.o
-$CC $LDFLAGS -o out/redo-ifchange out/redo-ifchange.o out/util.o out/build.o out/filepath.o
+$CC -o out/redo out/redo.o out/util.o out/build.o out/filepath.o $LDFLAGS
+$CC -o out/redo-ifchange out/redo-ifchange.o out/util.o out/build.o out/filepath.o $LDFLAGS
 
 # TODO: just for convenience, should be removed as soon as redo can build itself
 sudo install out/redo out/redo-ifchange /usr/bin/
