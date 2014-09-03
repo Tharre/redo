Tharre/redo

commit 5e5e7e66ee876e2aa62ac336b4e3c7b8ea6af1ce

Make the CC script not use any extra temp files

out/default.o.do
@@ -1,2 +1,2 @@
 redo-ifchange CC
-. ./CC "$@"
+./CC "$@"

out/CC.do
@@ -4,9 +4,9 @@ redo-ifchange config.sh
 exec >$3
 cat <<-EOF
 	redo-ifchange \$SRCDIR/\$2.c
-	$CC $CFLAGS -MD -MF \$3.deps -o \$3 -c \$SRCDIR/\$2.c
-	read DEPS <\$3.deps
-	rm -f \$3.deps
+	IFS= read DEPS << END
+	\$($CC $CFLAGS -MD -MF /dev/fd/1 -o \$3 -c \$SRCDIR/\$2.c)
+	END
 	redo-ifchange \${DEPS#*:}
 EOF
 chmod +x $3

commit a35218e77e279c3a856f35e15fcd95bcb8444e57

Fix regression caused by 5e5e7e6
We need to use the source operator on the CC script as if we don't
bad return codes won't get forwarded and the script won't get aborted.

out/default.o.do
@@ -1,2 +1,2 @@
 redo-ifchange CC
-./CC "$@"
+. ./CC "$@"